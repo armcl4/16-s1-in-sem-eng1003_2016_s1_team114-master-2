21f3994bbee88ae0b5a3327e0f6cbb318a8de95e
Abbey Rose Mclean <armcl4@student.monash.edu>
2016-05-24 11:43:46 +1000

    This is some extra code

---
+++ README.md
@@ -0,0 +1,2 @@
+# 16-s1-in-sem-eng1003_2016_s1_team114
+16-s1-in-sem-eng1003_2016_s1_team114
---
+++ addlocationPage.js
@@ -0,0 +1,2 @@
+// Code for the Add Location page.
+//code to get nickname, longitude, latitude for addLocation method in locationWeatherCache.js
