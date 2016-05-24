f2a9095046372baabc72de328fadae8062d59791
Abbey Rose Mclean <armcl4@student.monash.edu>
2016-05-24 11:49:37 +1000

    UP TO DATE ABBEY COMP

--- README.md
+++
@@ -1,2 +0,0 @@
-# 16-s1-in-sem-eng1003_2016_s1_team114
-16-s1-in-sem-eng1003_2016_s1_team114
--- addlocationPage.js
+++
@@ -1,2 +0,0 @@
-// Code for the Add Location page.
-//code to get nickname, longitude, latitude for addLocation method in locationWeatherCache.js
